e9a8910a2740ee2cd8d9cea8b83d7daf6ff16d2c
Dylan <tokotchd@gmail.com>
2019-03-01 23:55:15 +0000

    Fixed Async Iterator Issues, removed unnecessary dictionary, and began the !poll command

--- bot.py
+++ bot.py
@@ -1,46 +1,48 @@
 import discord
 import datetime
 
-TOKEN = 'NTQ5OTcxNzY1MjAwNDIwODk2.D1dzxA.4JBwpWzUMzz7VyNPr6QFHs2bXss'
+TOKEN = ''
 
 client = discord.Client()
+THREAD_TIMEOUT = datetime.timedelta(seconds=10)
+THREAD_TIMEOUT_CHECK = datetime.timedelta(hours=24)
 currentThreads = {}
-THREAD_TIMEOUT = datetime.timedelta(days=1)
+
+lastPurgeCheck = datetime.datetime.utcnow()
 
 @client.event
 async def on_message(message):
-    # here we update "last used" specifications on a thread
-    if message.channel.name.startswith('thread'):
-        currentThreads[message.channel] = datetime.datetime.now()
-
-    # here we delete any threads that haven't been used in at least the thread timeout duration.
-    for channel in client.get_all_channels():
-        if channel.name.startswith('thread'):
-            if datetime.datetime.now() - currentThreads[channel] > THREAD_TIMEOUT:
-                await client.delete_channel(channel)
-                del(currentThreads[channel])
-
-    #otherwise, we parse the message for commands
+    # Don't parse anything the bot says
     if message.author == client.user:
         return
 
+    # here we check to make sure that we only run the check to purge expired threads once every N seconds
+    global lastPurgeCheck
+    if datetime.datetime.utcnow() - lastPurgeCheck > THREAD_TIMEOUT_CHECK:
+        currentChannels = list(client.get_all_channels()) # copied so we don't have runtime iterator concerns
+        # here we delete any threads that haven't been used in at least the thread timeout duration
+        for channel in currentChannels:
+            if channel.name.startswith('thread'):
+                messages = client.logs_from(channel, limit=1, reverse=False)
+                lastMessage = None
+                async for message in messages:
+                    lastMessage = message
+                if datetime.datetime.utcnow() - message.timestamp > THREAD_TIMEOUT:
+                    await client.delete_channel(channel)
+        lastPurgeCheck = datetime.datetime.utcnow()
+
+    # after we've done the purge check, we should then check the message for commands
     if message.content.startswith('!thread'):
         content_split = message.content.split()
         if len(content_split) == 1:
-            channel_name = str(datetime.datetime.now())
+            msg = 'Please include a thread title in your thread create command.'
+            await client.send_message(message.channel, msg)
+            return
         else:
             channel_name = content_split[1]
             channel_members = content_split[2:]
 
         server = message.server
-
-        #creates a new public channel
-        #new_channel = await client.create_channel(server, 'thread-' + channel_name, type = discord.ChannelType.text)
-
-        #creates a new private channel
-        # everyone = discord.PermissionOverwrite(read_messages=False)
-        # mine = discord.PermissionOverwrite(read_messages=True)
-        # new_channel = await client.create_channel(server, 'thread-' + channel_name, (server.default_role, everyone), (server.me, mine))
 
         #creates a new channel with copied permissions from current channel
         rolesAndPermissions = {}
@@ -60,10 +62,16 @@ async def on_message(message):
         await client.send_message(new_channel, msg)
         msg = 'This thread will be closed automagically after 24 hours of inactivity, please copy what you want to keep!'
         await client.send_message(new_channel, msg)
-        currentThreads[new_channel] = datetime.datetime.now()
 
-        for currentThread, lastAccessed in currentThreads.items():
-            print(currentThread, datetime.datetime.now() - lastAccessed)
+    # here's the poll command
+    if message.content.startswith('!poll'):
+        content_split = message.content.split()
+        if len(content_split) < 3:
+            msg = 'Please include at least 2 items to choose between in the poll command'
+            await client.send_message(message.channel, msg)
+            return
+        else:
+            poll_items = content_split[1:]
 
 @client.event
 async def on_ready():
@@ -71,10 +79,5 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-
-    # on the case of reboot, grab all of the current threads by looking at "thread" prefix and set their last access to now.
-    for channel in client.get_all_channels():
-        if channel.name.startswith('thread'):
-            currentThreads[channel] = datetime.datetime.now()
 
 client.run(TOKEN)
